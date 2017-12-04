3294f94ad235654139b15443916cbe28f0aa9dae
Nath Bunyapisitsopar <nutbun@gmail.com>
2017-12-04 11:35:04 +1100

    Seperate translate function to utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,25 @@
+import 'url-search-params-polyfill';
+import { GOOGLE_TRANSLATE_API, GOOGLE_KEY, GOOGLE_TRANSLATE_TARGET_LANGUAGE } from "../config";
+
+export async function translate(rawArticle) {
+    // create header and body
+    const headers = {
+        'content-type': 'application/x-www-form-urlencoded'
+    };
+    const paramsObject: any = {
+        key: GOOGLE_KEY,
+        target: GOOGLE_TRANSLATE_TARGET_LANGUAGE,
+        format: 'html',
+        q: rawArticle
+    }
+    const params = new URLSearchParams(paramsObject);
+    // call the api
+    const fetchOption: object = {
+        method: "POST",
+        headers,
+        body: params
+    }
+    const response = await fetch(GOOGLE_TRANSLATE_API, fetchOption);
+
+    return response.json();
+}
