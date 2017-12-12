344fb3468309fad41608f48c0edf600f3a3e51f0
Nath Bunyapisitsopar <nutbun@gmail.com>
2017-12-12 12:30:26 +1100

    Fix update job query and image's import function

--- src/newExtractArticle.ts
+++ src/newExtractArticle.ts
@@ -12,28 +12,30 @@ export interface IEventData {
 
 export default async (event) => {
   try {
+
     const graphcool = fromEvent(event)
     const api = graphcool.api('simple/v1')
     const { jobId } = event.data
     const job: IJob = await getJob(api, jobId)
     const url = job.url
-
     const extract = await extractArticle(url)
     const cleanHTML = await cleanPageHTML(extract.content)
     const replaceData = replaceImages(cleanHTML)
     const titleData = await translate(extract.title)
+
     const articleData = {
       images: [...replaceData.images, {
         ref: 'i_m',
         source: extract.lead_image_url,
       }],
       publishedDate: extract.date_published,
-      status: STATUS.TRANSLATING,
+      status: STATUS.ASSIGNING,
       title: titleData.data.translations[0].translatedText,
       url: job.url,
     }
-    // return { error: JSON.stringify(articleData) }
-    const updateResponse: IJob = await updateJob(api, jobId, articleData, replaceData.html, STATUS.READY)
+    const updateResponse: IJob = await updateJob(
+      api, jobId, job.article.id, articleData, replaceData.html, STATUS.ASSIGNING,
+    )
     return {
       data: {
         id: updateResponse.id,
@@ -49,17 +51,27 @@ export default async (event) => {
 async function updateJob(
   api: GraphQLClient,
   jobId: string,
-  articleData: object,
+  articleId: string,
+  articleData: any,
   rawArticle: string,
   status: STATUS,
 ): Promise<IJob> {
   const mutation = `
-    mutation insertExtractedData($jobId: ID!, $articleData: JobarticleArticle, $rawArticle: String, $status: STATUS){
+    mutation insertExtractedData(
+      $jobId: ID!, $articleId: ID!, $images: [ArticleimagesImage!],
+      $publishedDate: DateTime, $title: String, $url: String,
+      $rawArticle: String, $status: STATUS
+    ){
+      updateArticle(
+        id: $articleId, images: $images, publishedDate: $publishedDate, status: $status, title: $title, url: $url
+      ){
+        id
+        status
+      }
       updateJob(
         id: $jobId,
         rawArticle: $rawArticle,
         status: $status,
-        article: $articleData
       ){
         id
         status
@@ -68,10 +80,14 @@ async function updateJob(
     }
   `
   const variables = {
-    articleData,
+    articleId,
+    images: articleData.images,
     jobId,
+    publishedDate: articleData.publishedDate,
     rawArticle,
     status,
+    title: articleData.title,
+    url: articleData.url,
   }
 
   return api.request<{ updateJob: IJob }>(mutation, variables)
